d5f1fbee1192036c932607f8348bd4b3be4d66cc
WolfoIsBestWolf <max.haldim@gmail.com>
2023-02-11 18:25:29 +0100

    init beta

---
+++ MainMod.cs
@@ -0,0 +1,362 @@
+﻿using BepInEx;
+using UnityEngine;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using MoreSlugcats;
+using Expedition;
+using RWCustom;
+
+namespace ExpeditionExtraConfig
+{
+    [BepInPlugin("wolfo.ExpeditionExtraConfig", "ExpeditionExtraConfig", "1.0.0")]
+    public class ExpeditionExtraConfig : BaseUnityPlugin
+    {
+        public void OnEnable()
+        {
+            On.RainWorld.OnModsInit += AddConfigHook;
+            On.RainWorldGame.ctor += AddILHooks;
+
+            On.WorldLoader.OverseerSpawnConditions += SpearSunOverseer;
+            On.RainCycle.GetDesiredCycleLength += RivuletRainCycle;
+            On.Expedition.ExpeditionGame.IsMSCRoomScript += RivuletAllowPebblesBall;
+
+            On.SaveState.ctor += ChangeStartingKarma; 
+            On.Expedition.ExpeditionGame.PrepareExpedition += ExpeditionGame_PrepareExpedition;
+            On.SlugcatStats.SlugcatStartingKarma += SlugcatStats_SlugcatStartingKarma;
+
+            On.Expedition.AchievementChallenge.ValidForThisSlugcat += AchievementChallenge_ValidForThisSlugcat;
+            On.Expedition.PearlDeliveryChallenge.RegionPoints += PearlDeliveryChallenge_RegionPoints;
+            On.Expedition.ExpeditionGame.GetRegionWeight += ExpeditionGame_GetRegionWeight;
+
+
+            On.Player.ctor += StartWithStomachPearl;
+        }
+
+        private void StartWithStomachPearl(On.Player.orig_ctor orig, Player self, AbstractCreature abstractCreature, World world)
+        {
+            orig(self, abstractCreature, world);
+            Debug.Log(world.rainCycle.CycleProgression);
+            if (world.game.rainWorld.ExpeditionMode && world.game.rainWorld.progression.currentSaveState.cycleNumber == 0 && world.rainCycle.CycleProgression <= 2f)
+            {
+                Debug.Log("Pearl");
+                if (self.SlugCatClass == SlugcatStats.Name.Red)
+                {
+                    world.game.FirstRealizedPlayer.objectInStomach = new DataPearl.AbstractDataPearl(world, AbstractPhysicalObject.AbstractObjectType.DataPearl, null, abstractCreature.spawnDen, world.game.GetNewID(), -1, -1, null, DataPearl.AbstractDataPearl.DataPearlType.Red_stomach);
+                }
+                else if (self.SlugCatClass == MoreSlugcatsEnums.SlugcatStatsName.Rivulet)
+                {
+                    world.game.FirstRealizedPlayer.objectInStomach = new DataPearl.AbstractDataPearl(world, AbstractPhysicalObject.AbstractObjectType.DataPearl, null, abstractCreature.spawnDen, world.game.GetNewID(), -1, -1, null, MoreSlugcatsEnums.DataPearlType.Rivulet_stomach);
+                }
+                
+            }
+            Debug.Log("Player_ctor 222222222222222222222222222222222222222");
+        }
+
+        private bool RivuletAllowPebblesBall(On.Expedition.ExpeditionGame.orig_IsMSCRoomScript orig, UpdatableAndDeletable item)
+        {
+            if (item is MSCRoomSpecificScript.RM_CORE_EnergyCell)
+            {
+                return false;
+            }
+            return orig(item);
+        }
+
+
+        private void ExpeditionGame_PrepareExpedition(On.Expedition.ExpeditionGame.orig_PrepareExpedition orig)
+        {
+            orig(); 
+            Expedition.ExpeditionGame.tempKarma = EECSettings.cfgKarmaStart.Value;
+            if (ExpeditionData.slugcatPlayer == MoreSlugcatsEnums.SlugcatStatsName.Rivulet)
+            {
+
+            }
+            Debug.Log("ExpeditionGame_PrepareExpedition " + ExpeditionData.slugcatPlayer.value);
+        }
+
+        private void ChangeStartingKarma(On.SaveState.orig_ctor orig, SaveState self, SlugcatStats.Name saveStateNumber, PlayerProgression progression)
+        {
+            orig(self, saveStateNumber, progression);
+            if (Custom.rainWorld.ExpeditionMode)
+            {
+                self.deathPersistentSaveData.karmaCap = EECSettings.cfgKarmaCapStart.Value; 
+                //self.deathPersistentSaveData.karma = ExpeditionGame.tempKarma; //Temp Karma used to circumvent setting Karma each start
+                if (saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Rivulet && EECSettings.cfgRivuletShortCycles.Value)
+                {
+                    self.miscWorldSaveData.pebblesEnergyTaken = false;
+                }
+                else if (saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Artificer && EECSettings.cfgArtificerRobo.Value)
+                {
+                    self.hasRobo = true;
+                }
+            }
+        }
+
+
+        private int SlugcatStats_SlugcatStartingKarma(On.SlugcatStats.orig_SlugcatStartingKarma orig, SlugcatStats.Name slugcatNum)
+        {
+            if (RWCustom.Custom.rainWorld.ExpeditionMode)
+            {
+                if (ModManager.MSC && slugcatNum == MoreSlugcatsEnums.SlugcatStatsName.Artificer)
+                {
+                    return 4;
+                }
+                if (ModManager.MSC && slugcatNum == MoreSlugcatsEnums.SlugcatStatsName.Saint)
+                {
+                    return 4;
+                }
+                return EECSettings.cfgKarmaCapStart.Value;
+            }
+            return orig(slugcatNum);
+        }
+
+        private static int RivuletRainCycle(On.RainCycle.orig_GetDesiredCycleLength orig, RainCycle self)
+        {
+            if (self.world.game.rainWorld.ExpeditionMode && ModManager.MSC && !self.world.singleRoomWorld && (self.world.game.session as StoryGameSession).saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Rivulet)
+            {   
+                int num = orig(self);
+                float multi = EECSettings.cfgRivuletBonusRain.Value;
+
+                if (self.world.region.name == "VS" || self.world.region.name == "UW" || self.world.region.name == "SH" || self.world.region.name == "SB" || self.world.region.name == "SL")
+                {
+                    num = (int)(num * multi / 1.5f);
+                }
+                else
+                {
+                    num = (int)(num * multi);
+                }
+                return num;
+            }
+            return orig(self);
+        }
+
+        private bool SpearSunOverseer(On.WorldLoader.orig_OverseerSpawnConditions orig, WorldLoader self, SlugcatStats.Name character)
+        {
+            //World tempWorld = self.ReturnWorld();
+            if (ModManager.MSC && character == MoreSlugcatsEnums.SlugcatStatsName.Spear && RWCustom.Custom.rainWorld.ExpeditionMode)
+            {
+                if (self.ReturnWorld() != null && (self.ReturnWorld().region.name != "SS" || self.ReturnWorld().region.name != "DM"))
+                {
+                    return true;
+                }
+            }
+
+            return orig(self, character);
+        }
+
+        private void AddConfigHook(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+        {
+            orig(self);
+            Debug.Log("ExpeditionExtraConfig: Wolfo Mod Loaded");
+            MachineConnector.SetRegisteredOI("ExpeditionExtraConfig", EECSettings.instance);
+        }
+
+        private static void AddILHooks(On.RainWorldGame.orig_ctor orig, RainWorldGame game, ProcessManager manager)
+        {
+            Debug.Log("ExpdExConfig: IL Hooks being added");
+            if (EECSettings.cfgKarmaFlower.Value)
+            {
+                IL.Room.Loaded += EnableNaturalKarmaFlower;
+            }
+            if (EECSettings.cfgRivuletShortCycles.Value)
+            {
+                IL.RainCycle.ctor += RivuletShelterFailure;
+            }
+            if (EECSettings.cfgRivuletBonusRain.Value == 1)
+            {
+                On.RainCycle.GetDesiredCycleLength -= RivuletRainCycle;
+            }
+            if (EECSettings.cfgSaintAscendPoints.Value)
+            {
+                IL.Player.ClassMechanicsSaint += SaintAscendMurder;
+            }
+
+
+            IL.Menu.GhostEncounterScreen.GetDataFromGame += GhostEncounterScreen_GetDataFromGame; //Probably fine to always have this
+
+            IL.StoryGameSession.ctor += StoryGameSession_ctor; //Smth about limiting Karma
+            IL.World.SpawnGhost += World_SpawnGhost; //Ghost Encountering Mechanic hook
+
+            orig(game, manager);
+            On.RainWorldGame.ctor -= AddILHooks; //Seems fine to only ever run this once
+        }
+
+
+
+        private static void SaintAscendMurder(ILContext il)
+        {
+            ILCursor c = new(il);
+            if (c.TryGotoNext(MoveType.Before,
+            x => x.MatchIsinst("Creature"),
+            x => x.MatchCallOrCallvirt("Creature", "Die")
+            ))
+            {
+                //c.Index -= 1;
+
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate<Func<PhysicalObject, Player, PhysicalObject>>((creature, player) =>
+                {
+                    if (creature is Creature && !(creature as Creature).dead)
+                    {
+                        if (Custom.rainWorld.ExpeditionMode)
+                        {
+                            player.SessionRecord.AddKill((creature as Creature));
+                        }
+                    }
+                    return creature;
+                });
+                Debug.Log("aaa: Saint Murder Points Success"); //could be improved
+            }
+            else
+            {
+                Debug.Log("aaa: Saint Murder Points Fail");
+            }
+        }
+
+        private static void World_SpawnGhost(ILContext il)
+        {
+            ILCursor c = new(il);
+            if (c.TryGotoNext(MoveType.Before,
+            x => x.MatchLdsfld("ModManager", "Expedition"),
+            x => x.MatchBrfalse(out _),
+            x => x.MatchLdsfld("ModManager", "Expedition")
+            )) 
+            { 
+                Debug.Log(c);
+                c.RemoveRange(9);
+                Debug.Log(c);
+                Debug.Log("aaa: Ghost Encounter IL Success");
+            }
+            else
+            {
+                Debug.Log("aaa: Ghost Encounter IL Failed");
+            }
+        }
+
+        private static void GhostEncounterScreen_GetDataFromGame(ILContext il)
+        {
+            ILCursor c = new(il);
+            c.TryGotoNext(MoveType.Before,
+            x => x.MatchLdsfld("ModManager", "Expedition"));
+
+            if (c.TryGotoNext(MoveType.Before,
+            x => x.MatchLdflda("Menu.KarmaLadderScreen/SleepDeathScreenDataPackage", "karma")))
+            {
+                c.Index -= 1;
+                Debug.Log(c);
+                c.RemoveRange(8);
+                Debug.Log(c);
+                Debug.Log("aaa: GhostEncounterScreen IL Success");
+            }
+            else
+            {
+                Debug.Log("aaa: GhostEncounterScreen IL Failed");
+            }
+
+        }
+
+        private static void StoryGameSession_ctor(ILContext il)
+        {
+            ILCursor c = new(il);
+            c.TryGotoNext(MoveType.Before,
+            x => x.MatchLdsfld("ModManager", "Expedition"));
+
+            if (c.TryGotoNext(MoveType.Before,
+            x => x.MatchLdfld("StoryGameSession", "saveState")))
+            {
+                c.Index -= 1;
+                Debug.Log(c);
+                c.RemoveRange(16);
+                Debug.Log(c);
+                Debug.Log("aaa: StoryGameSession_ctor IL Success");
+            }
+            else
+            {
+                Debug.Log("aaa: StoryGameSession_ctorIL Failed");
+            }
+        }
+
+
+        private static void RivuletShelterFailure(ILContext il)
+        {
+            ILCursor c = new(il);
+            c.TryGotoNext(MoveType.Before,
+            x => x.MatchLdfld("MiscWorldSaveData", "pebblesEnergyTaken"));
+            if (c.TryGotoNext(MoveType.Before,
+                x => x.MatchLdfld("DeathPersistentSaveData", "altEnding")))
+            {
+                c.Index += 1;
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate<Func<bool, RainCycle, bool>>((Check, self) =>
+                {
+                    if (self.world.game.rainWorld.ExpeditionMode)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return self.world.game.GetStorySession.saveState.deathPersistentSaveData.altEnding;
+                    }
+                });
+                Debug.Log("aaa: Rivulet PreCycleChance Hook Success");
+            }
+            else
+            {
+                Debug.Log("aaa: Rivulet PreCycleChance Hook Failed");
+            }
+        }
+
+
+        public static void EnableNaturalKarmaFlower(ILContext il)
+        {
+            ILCursor c = new(il);
+            if (c.TryGotoNext(MoveType.Before,
+                x => x.MatchLdstr("Preventing natural KarmaFlower spawn")))
+            {
+                c.TryGotoPrev(MoveType.Before,
+                x => x.MatchLdsfld("ModManager", "Expedition"));
+                c.Index += 1;
+                c.Next.OpCode = OpCodes.Brtrue_S;
+                Debug.Log("aaa: Karma Flower Hook Succeeded");
+            }
+            else
+            {
+                Debug.Log("aaa: Karma Flower Hook Failed");
+            }
+        }
+
+        private int ExpeditionGame_GetRegionWeight(On.Expedition.ExpeditionGame.orig_GetRegionWeight orig, string region)
+        {
+            if (region == "GW" && ExpeditionData.slugcatPlayer == MoreSlugcatsEnums.SlugcatStatsName.Spear)
+            {
+                return 6;
+            }
+            return orig(region);
+        }
+
+        private int PearlDeliveryChallenge_RegionPoints(On.Expedition.PearlDeliveryChallenge.orig_RegionPoints orig, PearlDeliveryChallenge self)
+        {
+            if (self.region == "MS")
+            {
+                return 35;
+            }
+            return orig(self);
+        }
+
+        private bool AchievementChallenge_ValidForThisSlugcat(On.Expedition.AchievementChallenge.orig_ValidForThisSlugcat orig, Expedition.AchievementChallenge self, SlugcatStats.Name slugcat)
+        {
+            if (((ModManager.MSC && slugcat == MoreSlugcatsEnums.SlugcatStatsName.Saint) || (!ModManager.MSC && slugcat == SlugcatStats.Name.Yellow)) && self.ID == WinState.EndgameID.Scholar)
+            {
+                return false;
+            }
+            else if (ModManager.MSC && slugcat == MoreSlugcatsEnums.SlugcatStatsName.Saint && self.ID == WinState.EndgameID.Hunter)
+            {
+                return false;
+            }
+            return orig(self, slugcat);
+        }
+
+    }
+}
---
+++ ModOptions.cs
@@ -0,0 +1,237 @@
+﻿using BepInEx;
+using UnityEngine;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using MoreSlugcats;
+using Menu.Remix.MixedUI;
+
+namespace ExpeditionExtraConfig
+{
+    public class EECSettings : OptionInterface
+    {
+	
+
+		public static EECSettings instance = new EECSettings();
+
+		
+		public static Configurable<bool> cfgKarmaFlower = instance.config.Bind("cfgKarmaFlower", true, new ConfigurableInfo("Normally Karma Flowers do not spawn. This will make expeditions significantly easier. Vanilla is false.", null, "", new object[]
+			{
+				"Natural Karma Flowers"
+			}));
+		public static Configurable<bool> cfgGhostsIncreaseKarma = instance.config.Bind("cfgGhostsIncreaseKarma", true, new ConfigurableInfo("Normally Echos do not increase Max Karma. This will make expeditions somewhat easier and allow Saint to reach his potential", null, "", new object[]
+			{
+				"Echos increase max Karma"
+			}));
+
+		public static Configurable<int> cfgKarmaStart = instance.config.Bind("cfgKarmaStart", 1, new ConfigurableInfo("Change starting Karma. Vanilla is 1. Ranges from 0 to 9", new ConfigAcceptableRange<int>(0, 9), "", new object[]
+			{
+				"Starting Karma"
+			}));
+		public static Configurable<int> cfgKarmaCapStart = instance.config.Bind("cfgKarmaCapStart", 4, new ConfigurableInfo("Change starting Karma. Vanilla is 4. Ranges from 0 to 9", new ConfigAcceptableRange<int>(0, 9), "", new object[]
+			{
+				"Starting Karma Cap"
+			}));
+
+
+		public static Configurable<bool> cfgArtificerRobo = instance.config.Bind("cfgArtificerRobo", true, new ConfigurableInfo("Artificer starts with his drone. This should be purely cosmetic", null, "", new object[]
+			{
+				"Artificer Drone"
+			}));
+		public static Configurable<bool> cfgSpearOverseer = instance.config.Bind("cfgSpearOverseer", true, new ConfigurableInfo("Spearmaster will be followed by his Red Overseer. This has purely cosmetic.", null, "", new object[]
+			{
+				"Spearmaster Overseer"
+			}));
+		public static Configurable<bool> cfgRivuletShortCycles = instance.config.Bind("cfgRivuletShortCycles", true, new ConfigurableInfo("Shorter Rain Cycles and more Shelter Failures like the start of Rivulets campaign. The Refract Core will be in it's usual place. Challenges like Points gained in a cycle will be made more difficult if not impossible unless you steal the Refract Core", null, "", new object[]
+			{
+				"Rivulet Shorter Cycles" 
+			}));
+		public static Configurable<float> cfgRivuletBonusRain = instance.config.Bind("cfgRivuletBonusRain", 1f, new ConfigurableInfo("Multiplies Rivulets shorter Cycles up to the duration of the normal cycles.", new ConfigAcceptableRange<float>(1f, 3f), "", new object[]
+			{
+				"Rivulet Lesser Short Cycles"
+			}));
+		public static Configurable<bool> cfgSaintAscendPoints = instance.config.Bind("cfgSaintAscendPoints", true, new ConfigurableInfo("Ascending things will grant points. This can make combat challenges a cakewalk. Level 10 Karma can only be gotten if other settings are enabled.", null, "", new object[]
+			{
+				"Saint Ascension gives Points"
+			}));
+
+		public static Configurable<bool> cfgStomachPearl = instance.config.Bind("cfgStomachPearl", true, new ConfigurableInfo("Hunter and Rivulet start with their Pearl as a headstart for Chieftain or other challenges", null, "", new object[]
+		{
+				"Stomach Pearl"
+		}));
+
+
+
+
+
+		public override void Initialize()
+		{
+			base.Initialize();
+			this.Tabs = new OpTab[]
+			{
+				new OpTab(this, "Options")
+			};
+			this.AddCheckbox();
+		}
+
+
+
+
+
+		private void AddCheckbox()
+		{
+			//0,0 is bottom left and looks fine
+			//575,575 is top right limit
+			//Consider 50 for Y
+			//+2.5y for Labels +35x for Labels Checkboxes
+			//half of SizeX for UpDowns
+			//Xsize defined in UpDowns
+			float lableY = 2.5f;
+			float labelX = 35f;
+			float stackY = 40f;
+			int stk = 0;
+			//float stack = 0;
+
+			float YLayer = 500;
+			float XLayer = 75;
+			
+
+			OpUpdown OptKarmaStart = new OpUpdown(cfgKarmaStart, new Vector2(XLayer -25, YLayer- stackY * stk), 50f)
+			{
+				description = cfgKarmaStart.info.description
+			};
+			OpLabel LabelKarmaStart = new OpLabel(XLayer + labelX, YLayer + lableY, cfgKarmaStart.info.Tags[0] as string, false)
+			{
+				description = cfgKarmaStart.info.description
+			};
+			stk++;
+			OpUpdown OptKarmaCapStart = new OpUpdown(cfgKarmaCapStart, new Vector2(XLayer-25, YLayer - stackY*stk), 50f)
+			{
+				description = cfgKarmaCapStart.info.description
+			};
+			OpLabel LabelKarmaCapStart = new OpLabel(XLayer + labelX, YLayer + lableY - stackY*stk, cfgKarmaCapStart.info.Tags[0] as string, false)
+			{
+				description = cfgKarmaCapStart.info.description
+			};
+			stk++;
+			OpCheckBox OptKarmaFlower = new OpCheckBox(cfgKarmaFlower, new Vector2(XLayer, YLayer - stackY*stk))
+			{
+				description = cfgKarmaFlower.info.description,
+			};
+			OpLabel LabelKarmaFlower = new OpLabel(XLayer + labelX, YLayer + lableY - stackY*stk, cfgKarmaFlower.info.Tags[0] as string, false)
+			{
+				description = cfgKarmaFlower.info.description,
+			};
+			stk++;
+			OpCheckBox OptGhostsIncreaseKarma = new OpCheckBox(cfgGhostsIncreaseKarma, new Vector2(XLayer, YLayer - stackY*stk))
+			{
+				description = cfgGhostsIncreaseKarma.info.description,
+			};
+			OpLabel LabelGhostsIncreaseKarma = new OpLabel(XLayer + labelX, YLayer + lableY - stackY*stk, cfgGhostsIncreaseKarma.info.Tags[0] as string, false)
+			{
+				description = cfgGhostsIncreaseKarma.info.description,
+			};
+
+
+			XLayer = 325f;
+			stk = 0;
+			OpCheckBox OptStomachPearl = new OpCheckBox(cfgStomachPearl, new Vector2(XLayer, YLayer - stackY * stk))
+			{
+				description = cfgStomachPearl.info.description,
+				colorEdge = PlayerGraphics.DefaultSlugcatColor(SlugcatStats.Name.Red),
+			};
+			OpLabel LabelStomachPearl = new OpLabel(XLayer + labelX, YLayer + lableY - stackY * stk, cfgStomachPearl.info.Tags[0] as string, false)
+			{
+				description = cfgStomachPearl.info.description,
+				color = PlayerGraphics.DefaultSlugcatColor(SlugcatStats.Name.Red)
+			};
+			stk++;
+			OpCheckBox OptArtificerRobo = new OpCheckBox(cfgArtificerRobo, new Vector2(XLayer, YLayer - stackY * stk))
+			{
+				description = cfgArtificerRobo.info.description,
+				colorEdge = PlayerGraphics.DefaultSlugcatColor(MoreSlugcatsEnums.SlugcatStatsName.Artificer),
+			};
+			OpLabel LabelArtificerRobo = new OpLabel(XLayer + labelX, YLayer + lableY - stackY * stk, cfgArtificerRobo.info.Tags[0] as string, false)
+			{
+				description = cfgArtificerRobo.info.description,
+				color = PlayerGraphics.DefaultSlugcatColor(MoreSlugcatsEnums.SlugcatStatsName.Artificer)
+			};
+			stk++;
+			OpCheckBox OptSpearOverseer = new OpCheckBox(cfgSpearOverseer, new Vector2(XLayer, YLayer - stackY*stk))
+			{
+				description = cfgSpearOverseer.info.description,
+				colorEdge = PlayerGraphics.DefaultSlugcatColor(MoreSlugcatsEnums.SlugcatStatsName.Spear),
+			};
+			OpLabel LabelSpearOverseer = new OpLabel(XLayer + labelX, YLayer + lableY - stackY*stk, cfgSpearOverseer.info.Tags[0] as string, false)
+			{
+				description = cfgSpearOverseer.info.description,
+				color = PlayerGraphics.DefaultSlugcatColor(MoreSlugcatsEnums.SlugcatStatsName.Spear)
+			};
+
+			stk++;
+			OpCheckBox OptRivRain = new OpCheckBox(cfgRivuletShortCycles, new Vector2(XLayer, YLayer - stackY*stk))
+			{
+				description = cfgRivuletShortCycles.info.description,
+				colorEdge = PlayerGraphics.DefaultSlugcatColor(MoreSlugcatsEnums.SlugcatStatsName.Rivulet),
+			};
+			OpLabel LabelRivRain = new OpLabel(XLayer + labelX, YLayer + lableY - stackY*stk, cfgRivuletShortCycles.info.Tags[0] as string, false)
+			{
+				description = cfgRivuletShortCycles.info.description,
+				color = PlayerGraphics.DefaultSlugcatColor(MoreSlugcatsEnums.SlugcatStatsName.Rivulet)
+			};
+
+			stk++;
+			OpUpdown OptRivRain2 = new OpUpdown(cfgRivuletBonusRain, new Vector2(XLayer-25f, YLayer - stackY*stk), 50f)
+			{
+				description = cfgRivuletBonusRain.info.description,
+				colorEdge = PlayerGraphics.DefaultSlugcatColor(MoreSlugcatsEnums.SlugcatStatsName.Rivulet),
+			};
+			OpLabel LabelRivRain2 = new OpLabel(XLayer + labelX, YLayer + lableY - stackY*stk, cfgRivuletBonusRain.info.Tags[0] as string, false)
+			{
+				description = cfgRivuletBonusRain.info.description,
+				color = PlayerGraphics.DefaultSlugcatColor(MoreSlugcatsEnums.SlugcatStatsName.Rivulet)
+			};
+
+			stk++;
+			OpCheckBox OptSaintPoints = new OpCheckBox(cfgSaintAscendPoints, new Vector2(XLayer, YLayer - stackY*stk))
+			{
+				description = cfgSaintAscendPoints.info.description,
+				colorEdge = PlayerGraphics.DefaultSlugcatColor(MoreSlugcatsEnums.SlugcatStatsName.Saint),
+			};
+			OpLabel LabelSaintPoints = new OpLabel(XLayer + labelX, YLayer + lableY - stackY*stk, cfgSaintAscendPoints.info.Tags[0] as string, false)
+			{
+				description = cfgSaintAscendPoints.info.description,
+				color = PlayerGraphics.DefaultSlugcatColor(MoreSlugcatsEnums.SlugcatStatsName.Saint)
+			};
+
+			this.Tabs[0].AddItems(new UIelement[]
+			{
+				OptKarmaStart,
+				LabelKarmaStart,
+				OptKarmaCapStart,
+				LabelKarmaCapStart,
+				OptKarmaFlower,
+				LabelKarmaFlower,
+				OptGhostsIncreaseKarma,
+				LabelGhostsIncreaseKarma,
+				OptStomachPearl,
+				LabelStomachPearl,
+				OptArtificerRobo,
+				LabelArtificerRobo,
+				OptSpearOverseer,
+				LabelSpearOverseer,
+				OptRivRain,
+				LabelRivRain,
+				OptRivRain2,
+				LabelRivRain2,
+				OptSaintPoints,
+				LabelSaintPoints
+			});
+		}
+
+
+
+	}
+}
